336bf869a691a3a04330bec72b6c3ca5fcd2a4ab
William cranney <william.cranney@gmail.com>
2018-03-06 02:44:33 +1100

    fixed inconsistency in anaconda version calls

--- ds-instance/startup.sh
+++ ds-instance/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
-bash Anaconda3-4.0.0-Linux-x86_64.sh -b -p $HOME
+bash Anaconda3-5.0.1-Linux-x86_64.sh -b -p $HOME
 export
 PATH=$HOME/bin:$PATH
 conda install scikit-learn
