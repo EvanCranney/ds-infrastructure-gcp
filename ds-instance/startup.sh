80ef831877263062ef1ba5e4fcca15e326510426
William cranney <william.cranney@gmail.com>
2018-03-06 02:54:10 +1100

    Fixed scikit install command, no longer requires confirmation

--- ds-instance/startup.sh
+++ ds-instance/startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
 bash Anaconda3-5.0.1-Linux-x86_64.sh -b -p $HOME
-export
-PATH=$HOME/bin:$PATH
-conda install scikit-learn
+export PATH=~/anaconda3/bin:$PATH
+conda install -y scikit-learn
