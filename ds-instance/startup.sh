b472727bcf22ba7931e38097ec36e5a5bdfbd227
Evan <ecra93@gmail.com>
2018-03-06 03:03:51 +1100

    Fixed bug: wrong path for Anaconda home

--- ds-instance/startup.sh
+++ ds-instance/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+# install Anaconda3
 wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
-bash Anaconda3-5.0.1-Linux-x86_64.sh -b -p $HOME
-export PATH=~/anaconda3/bin:$PATH
+bash Anaconda3-5.0.1-Linux-x86_64.sh -b -p $HOME/Anaconda3
+
+# install scikit-learn
+export PATH=$HOME/anaconda3/bin:$PATH
 conda install -y scikit-learn
