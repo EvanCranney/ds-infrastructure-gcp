ba3c2e11fb8e753e12873ea938e75694b51f4a12
Evan <ecra93@gmail.com>
2018-03-07 00:12:27 +1100

    Change default Python to Anaconda3 distribution

--- ds-instance/startup.sh
+++ ds-instance/startup.sh
@@ -16,8 +16,11 @@ cd /home/user
 wget https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
 bash Anaconda3-5.0.1-Linux-x86_64.sh -b -p /home/user/anaconda3
 
+# permanently change the python default path
+echo "export PATH=/home/user/anaconda3/bin:\$PATH" >> .profile
+source .profile
+
 # install scikit-learn
-export PATH=/home/user/anaconda3/bin:$PATH
 conda install -y scikit-learn
 
 EOF
